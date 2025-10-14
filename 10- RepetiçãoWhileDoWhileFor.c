36ae170c270fb0610f3b5b392f4f99b9c2834cc2
FelipePallandri <felipe.pallandri.rp@gmail.com>
2025-10-14 14:17:28 +0000

    atualizado

---
+++ 10- RepetiçãoWhileDoWhileFor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main (){
+
+// Estrutura de repetição While - Termina quando a condição é verdadeira
+
+    /*int i = 1;
+
+    while (i <= 100)
+        {
+            printf("%d\n", i);
+            i++;
+        }*/
+
+// Estrutura de repetição Do-While - Imprimi uma vez e depois executa ate a condiçao ser verdadeira
+
+    /*int i = 1;
+
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 5);*/
+
+// Estrutura de repetição For - começo, meio e fim (inicialização, condição e incremento)
+
+    for (int i = 1; i <= 26; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+
+}
